7668fc30c680d4a18499bd08ae90312367d7923c
Pablo Gonzalez <you@example.com>
2021-03-08 11:52:14 -0300

    arreglos para el desafio 2

--- js/app.js
+++ js/app.js
@@ -21,5 +21,5 @@ alert("Esto se parece a un paseo en bici, No es un entrenamiento para competir!!
 }
 
 else {
-alert("estoy confundido, no se que deporte estas practicando!")
+alert("estoy confundido, no se que deporte estas practicando!");
 }
