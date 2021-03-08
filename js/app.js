902f59664a10284a82df6dc3306f436034421ca2
Pablo Gonzalez <you@example.com>
2021-03-08 11:32:42 -0300

    practicando para el desafio 2 de JS

--- js/app.js
+++ js/app.js
@@ -1,3 +1,25 @@
-let tiempoLLuvioso = prompt("Su Plan favorito en dias lluviosos");
-console.log("Sin sol");
-alert("¡Es un muy buen plan!");
+let km_entrenados= prompt("Cuantos km entrenaste esta semana?")
+let  horas_entrenadas= prompt("cuantas horas por semana entrenas?")
+
+
+
+
+if (km_entrenados >250 && horas_entrenadas <=9) {
+alert("Muy buen entrenamiento, Segui asi campeon!!!");
+}
+
+else if (( km_entrenados >=200 &&  km_entrenados<=250) && (horas_entrenadas <=12)) {
+alert("Buen entrenamiento, la proxima semana podes mejorar!!!");
+}
+
+else if (( km_entrenados >=200 &&  km_entrenados<=250) && (horas_entrenadas >12 && horas_entrenadas <=18)) {
+alert("Buen entrenamiento, tenes que hacerlo en menos horas,acelera el ritmo");
+}
+
+else if (( km_entrenados >=100 && km_entrenados<200)) {
+alert("Esto se parece a un paseo en bici, No es un entrenamiento para competir!!!");
+}
+
+else {
+alert("estoy confundido, no se que deporte estas practicando!")
+}
